f8ffb9e01d4b52a777b5c3a4dbb8066bac4cc19e
carolina <carolraul27@gmail.com>
2023-04-24 18:42:25 -0400

    server changes

---
+++ src/server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv){
+    //argv[1] = ip;
+    //argv[2] = asymmetric
+    return 0;
+}
